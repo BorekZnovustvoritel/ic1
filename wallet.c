2d2fd02ec4db06b129a3b66cc533d5a6adb6140f
BorekZnovustvoritel <tvojemamatemarada@seznam.cz>
2022-03-23 15:20:28 +0100

    Initial commit

--- wallet.c
+++ wallet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <python3.10/Python.h>
+
+#define MAX_LENGTH 10
 
 char *inputString(size_t *size){
     char *str;
@@ -16,69 +16,77 @@ char *inputString(size_t *size){
             str = realloc(str, sizeof(char)*(base+=16));
             if(!str)return str;
         }
-        printf("%c\n", ch);
     }
-    printf("Ziju\n");
     str[len++]='\0';
     *size = len;
-    printf("Furt ziju\n");
-    fflush(stdin);
     return realloc(str, sizeof(char)*len);
 }
-
-PyObject *import_name(const char *modname, const char *symbol)
-{
-    PyObject *u_name, *module;
-    u_name = PyUnicode_FromString(modname);
-    module = PyImport_Import(u_name);
-    Py_DECREF(u_name);
-
-    return PyObject_GetAttrString(module, symbol);
+int strcmp(const char* str1, const char* str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) return 1;
+        i++;
+    }
+    if (str1[i] == str2[i] && str1[i] == '\0') return 0;
+    return 1;
+}
+void strcopy(char *dest, char *src) {
+    unsigned int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
 }
 
 short authenticate(char *login, char *password) {
-    printf("hi");
-    sleep(1);
-    PyObject *func = import_name("auth", "auth");
-    PyObject *args;
-    PyObject *kwargs;
-    PyObject *result = 0;
-    short ans;
-
-    PyGILState_STATE state = PyGILState_Ensure();
-    if (!PyCallable_Check(func)) {
-        fprintf(stderr, "Could not call authentization service! Module cannot be run.");
-        return -1;
+    int isAdmin = 0;
+    char userData[2][MAX_LENGTH]; //both are overflowable
+    char data[2][MAX_LENGTH];
+    int charIndex = 0;
+    short nameOrPassword = 0; // 0 for name, 1 for password
+    strcopy(userData[0], login);
+    strcopy(userData[1], password);
+    FILE* in = fopen("stash", "r");
+    char ch;
+    while ((ch = fgetc(in)) != EOF) {
+        if (ch == '\n') {
+            data[1][charIndex] = '\0';
+            if (strcmp(login, data[0]) == 0) {
+                if (strcmp(password, data[1]) == 0) {
+                    fclose(in);
+                    return 1;
+                }
+            }
+            charIndex = 0;
+            nameOrPassword = 0;
+        }
+        else if (ch == ':') {
+            data[0][charIndex+1] = '\0';
+            charIndex = 0;
+            nameOrPassword = 1;
+        }
+        else {
+            if (charIndex < MAX_LENGTH-2) { //overflow from text loading is not possible, I hope
+                data[nameOrPassword][charIndex] = ch;
+                data[nameOrPassword][charIndex+1] = '\0';
+                charIndex++;
+            }
+        }
     }
-    args = Py_BuildValue("(ss)", login, password);
-    kwargs = NULL;
-    result = PyObject_Call(func, args, kwargs);
-    Py_DECREF(args);
-    Py_XDECREF(kwargs);
-    if (PyErr_Occurred()) {
-        fprintf(stderr, "Could not call authentization service! Module threw an error.");
-        PyErr_Print();
-        return -1;
-    }
-    if (!PyBool_Check(result)) {
-        fprintf(stderr, "Could not authenticate, module returns wrong data type!");
-        return -1;
-    }
-    ans = PyObject_IsTrue(result);
-    return ans;
+    fclose(in);
+    if (isAdmin) return 2;
+    printf("%d", isAdmin);
+    return 0;
 }
 
 int main() {
-    size_t loglen = 0;
+    long loglen = 0;
     printf("Login as: ");
     char* bufferlog = inputString(&loglen);
-    /*printf("login: %s, length: %d", bufferlog, loglen);
-    size_t passlen = 0;
+    long passlen = 0;
     printf("\nPassword: ");
     char* bufferpass = inputString(&passlen);
-    printf("\nhuh");
-    printf("password: %s, length: %d", bufferpass, passlen);
-    printf("\n\n\nNevim");*/
-    printf("Authentication value: %d", authenticate("kotek", "piesek"));
-    //fflush(stdout);
+    int ans = authenticate(bufferlog, bufferpass);
+    printf("\nAuthentication value: %d", ans);
+    return 0;
 }
